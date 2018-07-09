2767e40123bcef3776ec98d5351ee0e0fe57b9f7
José Manuel Lucas <josemanuel@jmlweb.es>
2018-07-09 22:23:56 +0200

    Destructuring

--- src/App.js
+++ src/App.js
@@ -2,8 +2,21 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const toPrint = 'Welcome to KeepCoding ' + Date.now();
-const toPrint2 = `Welcome to KeepCoding ${Date.now()}`;
+const user = {
+  name : 'José Manuel',
+  surname: 'Lucas Muñoz',
+  address : 'C\ de la Fantasía, 7, Pilar de la Muela (Mordor)',
+  phone: '666666666',
+};
+
+const { name, surname, address, phone } = user;
+
+const toPrint = `
+  Nombre: ${name};
+  Apellidos: ${surname};
+  Dirección: ${address};
+  Teléfono: ${phone};
+`;
 
 class App extends Component {
   render() {
@@ -11,7 +24,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">{toPrint2}</h1>
+          <h1 className="App-title">{toPrint}</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
