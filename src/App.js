859f7a18f3047ced572c98e444b0f451d411db74
José Manuel Lucas <josemanuel@jmlweb.es>
2018-07-09 23:12:17 +0200

    Ejemplo filter·

--- src/App.js
+++ src/App.js
@@ -4,10 +4,13 @@ import './App.css';
 
 const myArr = [1,2,3,4,5,6,7,8,9];
 
-const result1 = myArr.map((v, k) => {
-  const value = v * 2;
-  const append = k < myArr.length - 1 ? ', ' : '';
-  return `${value}${append}`;
+const result1 = myArr.map(v => v * 2);
+
+const result2 = myArr.filter(v => v % 2 === 0);
+
+const addAppend = arr => arr.map((v, k) => {
+  const append = k < arr.length - 1 ? ', ' : '';
+  return `${v}${append}`;
 });
 
 class App extends Component {
@@ -19,8 +22,9 @@ class App extends Component {
           <h1 className="App-title">KeepCoding</h1>
         </header>
         <div className="App-intro">
-          <p><b>myArr:</b> {myArr}</p>
-          <p><b>Result1:</b> {result1}</p>
+          <p><b>myArr:</b> {addAppend(myArr)}</p>
+          <p><b>Result1:</b> {addAppend(result1)}</p>
+          <p><b>Result2:</b> {addAppend(result2)}</p>
         </div>
       </div>
     );
