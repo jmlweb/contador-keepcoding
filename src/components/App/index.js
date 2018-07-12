bbcf605d18167d0f9b95799abc37f009e322f87a
José Manuel Lucas <josemanuel@jmlweb.es>
2018-07-12 20:53:59 +0200

    Counter

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -5,15 +5,22 @@ import CounterValue from '../CounterValue';
 import OperationButtons from '../OperationButtons';
 import ResetButtons from '../ResetButtons';
 
+const INITIAL_STATE = {
+  value: 0,
+};
+
 class App extends Component {
-  state = {
-    value: 10,
+  state = INITIAL_STATE;
+
+  updateCounter = (e) => {
+    const { value } = e.target;
+    this.setState(prevState => ({
+      value: prevState.value + +value,
+    }));
   };
 
   resetCounter = () => {
-    this.setState({
-      value: 0,
-    });
+    this.setState(INITIAL_STATE);
   };
 
   render() {
@@ -21,7 +28,7 @@ class App extends Component {
     return (
       <Template logo="https://keepcoding.io/es/wp-content/uploads/sites/4/2015/05/logo-keepcoding-web.png">
         <CounterValue value={value} />
-        {value > -10 && value < 10 ? <OperationButtons /> : <ResetButtons resetCounter={this.resetCounter} />}
+        {value > -10 && value < 10 ? <OperationButtons updateCounter={this.updateCounter} removeOne={this.removeOne} /> : <ResetButtons resetCounter={this.resetCounter} />}
       </Template>
     );
   }
--- src/components/OperationButtons/index.js
+++ src/components/OperationButtons/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import ButtonGroup from '../ButtonGroup';
 import Button from '../Button';
 
-const OperationButtons = () => (
+const OperationButtons = ({ updateCounter }) => (
   <ButtonGroup>
-    <Button type="button">
+    <Button type="button" onClick={updateCounter} value={-1}>
       -1
     </Button>
-    <Button type="button">
+    <Button type="button" onClick={updateCounter} value={1}>
       +1
     </Button>
   </ButtonGroup>
 );
+
+OperationButtons.propTypes = {
+  updateCounter: PropTypes.func.isRequired,
+};
 
 export default OperationButtons;
